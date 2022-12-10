c5b2476c714d718b9330ae13264133aca82e29bb
Tom P <flowerwarrior1@protonmail.com>
2022-12-10 22:10:03 +0100

    u

--- Assets/Others/AudioManager.cs
+++ Assets/Others/AudioManager.cs
@@ -56,6 +56,7 @@ public class AudioManager : MonoBehaviour
 
     private void PlayEarthKicked()
     {
+        print("kicked audio");
         PlayAudioEffect(earthKicked);
     }
 
--- Assets/TotalStarsMgr.cs
+++ Assets/TotalStarsMgr.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TotalStarsMgr : MonoBehaviour
 {
+    [SerializeField] Transform[] planets;
+    [SerializeField] TextMeshProUGUI textNextUnlock;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        int starsPerPlanet = 3;
+        int totalStars = PlayerPrefs.GetInt("totalStars", 0);
+        int planetsUnlocked = Mathf.FloorToInt(((float)totalStars / (float)starsPerPlanet));
+
+        for (int i = 0; i < planets.Length; i++)
+        {
+            planets[i].GetChild(0).gameObject.SetActive(false);
+            planets[i].GetChild(1).gameObject.SetActive(true);
+        }
+
+        for (int i = 0; i < planetsUnlocked; i++)
+        {
+            planets[i].GetChild(0).gameObject.SetActive(true);
+            planets[i].GetChild(1).gameObject.SetActive(false);
+        }
+
+        textNextUnlock.text = $"Collect {totalStars % starsPerPlanet} more to get next planet collectible.";
     }
 
     // Update is called once per frame
